38981179ce13ac0b67d4b07ccae0264e796f1b13
winebarrel <sgwr_dts@yahoo.co.jp>
2016-07-01 00:56:58 +0900

    Fix homebrew formula

--- homebrew/mypasswd.rb
+++ homebrew/mypasswd.rb
@@ -1,16 +1,17 @@
 require 'formula'
 
 class Mypasswd < Formula
-  VERSION = '0.1.0'
+  VERSION = '0.1.1'
 
   homepage 'https://github.com/winebarrel/mypasswd'
-  url "https://github.com/winebarrel/mypasswd/releases/download/v#{VERSION}/mypasswd-v#{VERSION}-darwin-amd64.gz"
-  sha256 'eb22e65b4cc0b79112f968418f3254b456c6704135cfea62bce6b9fc2fe44496'
+  url "https://github.com/winebarrel/mypasswd/archive/v0.1.1.tar.gz"
+  sha256 '9ce48fcc3e0395de8e1b59bbea0542d853a9c2f4abac2b4da9e7202f929d1517'
   version VERSION
   head 'https://github.com/winebarrel/mypasswd.git', :branch => 'master'
+  depends_on 'go' => :build
 
   def install
-    system "mv mypasswd-v#{VERSION}-darwin-amd64 mypasswd"
+    system "make"
     bin.install 'mypasswd'
   end
 end
